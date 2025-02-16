708f02af93abb96f2f41d35cd727293650f79953
SergioBonatto <bonatto@tutanota.com>
2025-02-16 03:31:48 -0300

    fix: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import SEO from '@/components/SEO';
 import Navbar from '@/components/Nav';
 import MusicPlayer from '@/components/musicPlayer';
