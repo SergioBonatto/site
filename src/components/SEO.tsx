210c9922e0ee4ad341870e5dd0b4d2b0f76db583
SergioBonatto <bonatto@tutanota.com>
2025-02-22 21:54:07 -0300

    update: SEO X

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -7,22 +7,28 @@ interface SEOProps {
   url: string;
 }
 
-const SEO: React.FC<SEOProps> = ({ title, description, image, url }) => (
-  <Head>
-    <meta property="og:image" content={image} />
-    <meta property="og:url" content={url} />
-    <meta property="og:site_name" content={title} />
-    <meta property="title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@fibonatto" />
-    <meta name="twitter:creator" content="@fibonatto" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content={image} />
-    <meta name="twitter:url" content={url} />
-    <meta name="twitter:image:alt" content={title} />
-  </Head>
-);
+const SEO: React.FC<SEOProps> = ({ title, description, image, url }) => {
+  const fullImageUrl = `https://bonatto.vercel.app${image}`;
+
+  return (
+    <Head>
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={fullImageUrl} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@fibonatto" />
+      <meta name="twitter:creator" content="@fibonatto" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={fullImageUrl} />
+    </Head>
+  );
+};
 
 export default SEO;
