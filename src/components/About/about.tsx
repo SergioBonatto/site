cb72c17292bc02eec004cfb6003351f4f9537877
SergioBonatto <bonatto@tutanota.com>
2025-06-29 04:04:28 -0300

    fix: about on my blog 2

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 interface AboutProps extends React.HTMLAttributes<HTMLElement> {
